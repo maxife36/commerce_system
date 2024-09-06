604bc218fb03a861f8b14afe0e010c431b85c3f1
maxife36 <maxife36@gmail.com>
2024-09-06 14:32:30 -0300

    add relationship between models Purchase & Supplier

--- models/purchases.go
+++ models/purchases.go
@@ -8,10 +8,10 @@ import (
 
 type Purchase struct {
 	gorm.Model
-	Details json.RawMessage `json:"details" gorm:"type:json"`
-	Amount  float64         `json:"amount" gorm:"type:decimal(10,2)"`
-	UserId  uint            `json:"-"`
-	User    User            `json:"user"`
-	//SupplierId uint `json:"-"`
-	//Supplier Supplier `json:"supplier"`
+	Details    json.RawMessage `json:"details" gorm:"type:json"`
+	Amount     float64         `json:"amount" gorm:"type:decimal(10,2)"`
+	UserId     uint            `json:"-"`
+	User       User            `json:"user"`
+	SupplierId uint            `json:"-"`
+	Supplier   Supplier        `json:"supplier"`
 }
