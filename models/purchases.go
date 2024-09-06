57f2945beced3f6c918e6e73f566ad363ec9c6a7
maxife36 <maxife36@gmail.com>
2024-09-06 14:17:19 -0300

    model Purchase

---
+++ models/purchases.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"encoding/json"
+
+	"gorm.io/gorm"
+)
+
+type Purchase struct {
+	gorm.Model
+	Details json.RawMessage `json:"details" gorm:"type:json"`
+	Amount  float64         `json:"amount" gorm:"type:decimal(10,2)"`
+	UserId  uint            `json:"-"`
+	User    User            `json:"user"`
+	//SupplierId uint `json:"-"`
+	//Supplier Supplier `json:"supplier"`
+}
